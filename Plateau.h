58575030f4fc150ec431c38c80b9d94ce78a2b27
argoniseur <ldeferluc@gmail.com>
2017-03-26 13:13:56 +0200

    Plateau.h

--- Plateau.h
+++ Plateau.h
@@ -6,4 +6,24 @@
 
 typedef struct s_board *Board;
 
-Board create_board(int N);
+Board init_B1(Board b, int n);
+
+Board init_B2(Board b, int n);
+
+Board init_W1(Board b, int n);
+
+Board init_W2(Board b, int n);
+
+bool cell_on_edges(Board b, Cell cell);
+
+bool surrounded(Board b, Cell cell);
+ 
+Board create_board();
+
+void board_delete(Board b);
+
+int get_cell_value(Board b, int index);
+
+Cell get_cell(Board b, int index);
+
+Board insert_cell_value(SkipList d, int index, int value);
