d8d8d91eac700799b808d9fd327c48af3f1be403
plumpmango <clemence-cb@hotmail.fr>
2017-05-12 17:08:10 +0200

    main

--- src/Hex.c
+++ src/Hex.c
@@ -3,14 +3,33 @@
 #include "menu.h"
 
 int main(int argc, char ** argv){
-    Board hexBoard = create_board();
+  int nbTurn;  
+  char game;
+    
+  game = newGame();
+  if(game=='O'){
+     Player p1; Player p2;
+     init_players(&p1,&p2);  
+  }
+  else
+    return 0;
+    
+  Board hexBoard=create_board(();
+  print_board(hexBoard);
 
-    char c = newGame();
-    printf("%c", c);
+  nbTurn=0;
+  Player p=p1;
+  while(nbTurn<N*N){
+    hexBoard = get_and_insert_coord(hexBoard,&p);
     print_board(hexBoard);
-    Player j1 = NULL, j2 = NULL;
-    initJoueurs(&j1,&j2);
-    hexBoard = get_and_insert_coord(hexBoard, &j1);
-    print_board(hexBoard);
+    
+    if(p==p1)
+      p=p2;
+    else
+      p=p1;
+    
+    nbTurn++;
+  }
+  
     return 0;
 }
