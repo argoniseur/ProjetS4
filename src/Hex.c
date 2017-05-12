97173d1741c59174a2945348a8cbe7a0783833e5
argoniseur <ldeferluc@gmail.com>
2017-05-12 15:16:33 +0200

    Hex.c

--- src/Hex.c
+++ src/Hex.c
@@ -1,6 +1,8 @@
 #include "Plateau.h"
+#include "Affichage.h"
 
 int main(int argc, char ** argv){
-    //Board hexBoard = create_board();
+    Board hexBoard = create_board();
+    print_board(hexBoard);
     return 0;
 }
