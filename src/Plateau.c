47835cab636cc05054a80e62e58038e44228437a
argoniseur <ldeferluc@gmail.com>
2017-05-12 15:10:07 +0200

    Plateau.c

--- src/Plateau.c
+++ src/Plateau.c
@@ -4,31 +4,52 @@
 #include "Plateau.h"
 #define N 11
 
-struct s_cell{
-	int value;
-	int index;
-	struct s_cell **neighboors;
-};
+#define E 0
+#define SE 1
+#define SO 2
+#define O 3
+#define NO 4
+#define NE 5
 
-struct s_board{
-	int size;
-	Cell tab[N*N];
-	Cell sentinelB1;
-	Cell sentinelB2;
-	Cell sentinelW1;
-	Cell sentinelW2;
-};
-
-/*Board init_B1(Board b, int n){
-    // assert
-    int val;
+//Bord gauche
+Board init_B1(Board b){
     for(int i=0;i<N;i++){
-        val = board_ith(b, i);
-        b->sentinelB1->edges[i] = val;
+        b->sentinelB1->neighboors[i] = b->tab[i*N];
+        b->tab[i*N]->neighboors[SO] = b->sentinelB1;
+        b->tab[i*N]->neighboors[O] = b->sentinelB1;
     }
     return b;
+}
 
-}*/
+//Bord droit
+Board init_B2(Board b){
+    for(int i=0;i<N;i++){
+        b->sentinelB2->neighboors[i] = b->tab[(i*N)+(N-1)];
+        b->tab[(i*N)+(N-1)]->neighboors[E] = b->sentinelB2;
+        b->tab[(i*N)+(N-1)]->neighboors[NE] = b->sentinelB2;
+    }
+    return b;
+}
+
+//Bord haut
+Board init_W1(Board b){
+    for(int i=0;i<N;i++){
+        b->sentinelW1->neighboors[i] = b->tab[i];
+        b->tab[i]->neighboors[NE] = b->sentinelW1;
+        b->tab[i]->neighboors[NO] = b->sentinelW1;
+    }
+    return b;
+}
+
+//Bord bas
+Board init_W2(Board b){
+    for(int i=0;i<N;i++){
+        b->sentinelB1->neighboors[i] = b->tab[(N*(N-1))+i];
+        b->tab[(N*(N-1))+i]->neighboors[SE] = b->sentinelW2;
+        b->tab[(N*(N-1))+i]->neighboors[SO] = b->sentinelW2;
+    }
+    return b;
+}
 
 
 /**Creation du plateau
@@ -37,128 +58,116 @@ struct s_board{
  */
 
   //Fonction de creation d'une Cellule
-  
-  Cell create_cell(int index){
-    
+Cell create_cell(int index){ 
     //allocation espace mémoire pour une cellule
-    Cell c=(Cell)malloc(sizeof(struct s_cell));
+    Cell c = (Cell)malloc(sizeof(struct s_cell));
     
-    c->index=index;
+    c->index = index;
     
     //initialisation du contenu de la cellule à 0 = case de plateau vide
-    c->value=0;
+    c->value = 0;
+    c->neighboors = (Cell *)malloc(6*sizeof(struct s_cell));
     return c;
-  }
+}
   
   //Fonction de création d'une sentinelle
-  
-  Cell create_sentinel(int val){
+Cell create_sentinel(int val){
+    Cell s = (Cell)malloc(sizeof(struct s_cell));
     
-    Cell s=(Cell)malloc(sizeof(struct s_cell));
-    
-    s->value=val;
-    
+    s->value = val;
+    s->index = -1;
     s->neighboors = (Cell *)malloc(N*(sizeof(Cell)));
 	return s;
-  }
+}
 
 //Fonction de création du plateau
-Board create_board(){
+Board create_board(){ 
     
-  Board b=(Board)malloc(sizeof(struct s_board));
+    Board b=(Board)malloc(sizeof(struct s_board));
     
-  b->size=N*N;
+    b->size = N*N;
     
- //Création de la sentinelle pour les bord supérieure et inférieure
-  b->sentinelB1=create_sentinel(1);
-  b->sentinelB2=create_sentinel(1);
+    //Création de la sentinelle pour les bord supérieure et inférieure
+    b->sentinelB1 = create_sentinel(3);
+    b->sentinelB2 = create_sentinel(1);
     
- //Création de la sentinelle pour les bord gauche et droite
-  b->sentinelW1=create_sentinel(2);
-  b->sentinelW2=create_sentinel(2);
+    //Création de la sentinelle pour les bord gauche et droite
+    b->sentinelW1 = create_sentinel(4);
+    b->sentinelW2 = create_sentinel(2);
     
-  for(int i=0;i<b->size;i++)
-    b->tab[i]=create_cell(i);
+    for(int i=0;i<b->size;i++)
+        b->tab[i] = create_cell(i);
       
- //Chainage des sentinel au cellules bordantes
-  //b->sentinelB1=init_B1(b,N);
-  //b->sentinelB2=init_B2(b,N);
-  //b->sentinelW1=init_W1(b,N);
-  //b->sentinelW2=init_W2(b,N);
-    
- //Chainage des cellules du palteau
-  for(int i=0;i<b->size;i++){
-      
-  //Case bordure supérieur
-    if(0<=(b->tab[i]->index) && (b->tab[i]->index)<N){
-      b->tab[i]->neighboors[3]=b->sentinelB1;
-      b->tab[i]->neighboors[2]=b->sentinelB1;
-    }
-    else{
-      b->tab[i]->neighboors[3]=b->tab[i-N+1];
-      b->tab[i]->neighboors[2]=b->tab[i-N];
-    }
-      
-  //Case bordure droite
-    if( ((b->tab[i]->index) % N) == N-1)
-      b->tab[i]->neighboors[0]=b->sentinelW2;
-    else
-      b->tab[i]->neighboors[0]=b->tab[i+1];
-      
- //Case bordure inférieure
-    if( ((b->size-N)>=b->tab[i]->index) && ((b->tab[i]->index)<b->size) ){
-      b->tab[i]->neighboors[5]=b->sentinelB2;
-      b->tab[i]->neighboors[4]=b->sentinelB2;
-    }
-    else{
-      b->tab[i]->neighboors[5]=b->tab[i+N];
-      b->tab[i]->neighboors[4]=b->tab[i+N-1];
-    }
-      
-    if( ((b->tab[i]->index) % N) == 0)
-      b->tab[i]->neighboors[1]=b->sentinelW1;
-    else
-      b->tab[i]->neighboors[1]=b->tab[i-1];
-      
-  }
-     
-     return b;
-}    
+    //Chainage des sentinel au cellules bordantes
+        b = init_B1(b);
+        b = init_B2(b);
+        b = init_W1(b);
+        b = init_W2(b);
 
-  
+    //Chainage des cellules du palteau
+    for(int i=0;i<b->size;i++){
+      
+        //chainage est
+        if((i+1)%N != 0)
+            b->tab[i]->neighboors[E] = b->tab[i+1];
+
+        //chainage sud est
+        if(i+N < N*N)
+            b->tab[i]->neighboors[SE] = b->tab[i+N];
+
+        //chainage sud ouest
+        if(i+N-1 < N*N)
+            b->tab[i]->neighboors[SO] = b->tab[i+N-1];
+
+        //chainage ouest
+        if(i%N != 0)
+            b->tab[i]->neighboors[O] = b->tab[i-1];
+
+        //chainage nord ouest
+        if(i-N > 0)
+            b->tab[i]->neighboors[NO] = b->tab[i-N];
+
+        //chainage nord est
+        if(i-N+1 > 0)
+            b->tab[i]->neighboors[NE] = b->tab[i-N+1];
+    }
+
+    return b;
+}
+
 /**Suppression du plateau
  * Par COURDY-BAHSOUN Clémence
  * Dernière modification le 15.04.2017
  */
   
-  //Destructon d'une cellule
+    //Destructon d'une cellule
   
-  void cell_delete(Cell c){
+void cell_delete(Cell c){
     free(c);
-  }
+}
 
   //Suppression d'une sentinelle
-  void sentinel_delete(Cell s){
+void sentinel_delete(Cell s){
     
     for(int i=0;i<N;i++)
-      cell_delete(s->neighboors[i]);
+        cell_delete(s->neighboors[i]);
     
     free(s->neighboors);
     
     free(s);
-  }
+}
 
 //Suppression du plateau
 void board_delete(Board b){
   
-  for(int i=0;i<b->size;i++)
-    cell_delete(b->tab[i]);
+    for(int i=0;i<b->size;i++)
+        cell_delete(b->tab[i]);
   
-  sentinel_delete(b->sentinelB1);
-  sentinel_delete(b->sentinelB2);
-  sentinel_delete(b->sentinelW1);
-  sentinel_delete(b->sentinelW2);
+    sentinel_delete(b->sentinelB1);
+    sentinel_delete(b->sentinelB2);
+    sentinel_delete(b->sentinelW1);
+    sentinel_delete(b->sentinelW2);
   
-  free(b);
+    free(b);
 }
 
